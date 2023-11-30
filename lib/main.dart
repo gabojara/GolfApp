724eb1fb54d0e3af2aebfee5f2c96c50b0f01d40
Gabriel <vera.gabrielalejandro@gmail.com>
2023-11-29 22:54:37 -0400

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,51 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  List<CameraDescription> cameras = await availableCameras();
-  runApp(MyApp(cameras: cameras));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<CameraDescription> cameras;
-
-  const MyApp({Key? key, required this.cameras}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(cameras: cameras),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final List<CameraDescription> cameras;
-
-  const MyHomePage({Key? key, required this.cameras}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  File? _imageFile;
+
+  Future<void> _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+    if (image != null) {
+      setState(() {
+        _imageFile = File(image.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Home'),
+        centerTitle: true,
         backgroundColor: Colors.black,
       ),
       bottomNavigationBar: BottomAppBar(
@@ -44,33 +54,63 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             IconButton(
-              icon: const Icon(Icons.home),
+              icon: const Icon(Icons.history, size: 35),
               onPressed: () {},
               color: Colors.white,
             ),
             IconButton(
-              icon: const Icon(Icons.camera_alt),
-              onPressed: () {
-                //
-              },
-              color: Colors.white,
-            ),
-            IconButton(
-              icon: const Icon(Icons.settings),
+              icon: const Icon(Icons.settings, size: 35),
               onPressed: () {},
               color: Colors.white,
             ),
           ],
         ),
       ),
+      body: Center(
+        widthFactor: 2.0,
+        heightFactor: 1.2,
+        child: _imageFile != null
+            ? _ZoomableImage(imageFile: _imageFile!)
+            : const Text(''),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //
-        },
+        onPressed: _takePicture,
         backgroundColor: Colors.black,
-        child: const Icon(Icons.add),
+        elevation: 10.0,
+        child: const Icon(Icons.camera_alt, size: 36),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _ZoomableImage extends StatefulWidget {
+  final File imageFile;
+
+  const _ZoomableImage({Key? key, required this.imageFile}) : super(key: key);
+
+  @override
+  __ZoomableImageState createState() => __ZoomableImageState();
+}
+
+class __ZoomableImageState extends State<_ZoomableImage> {
+  bool _isZoomed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isZoomed = !_isZoomed;
+        });
+      },
+      child: Container(
+        child: SizedBox(
+          width: _isZoomed ? null : 350.0,
+          height: _isZoomed ? null : 350.0,
+          child: Image.file(widget.imageFile),
+        ),
+      ),
     );
   }
 }
